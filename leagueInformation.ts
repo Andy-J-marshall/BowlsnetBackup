0bbcd1ff841775138638ddf1b35fd0c2637a5cf4
Andrew Marshall <stanningleybowlsclub@gmail.com>
2025-06-11 17:55:17 +0100

    added separate file to store league info

---
+++ leagueInformation.ts
@@ -0,0 +1,270 @@
+export const leagues = [
+  {
+    day: "Leeds Monday Combined",
+    url: "/Leeds-MonComb",
+  },
+  {
+    day: "Leeds Tuesday Vets",
+    url: "/Leeds-TueVets",
+  },
+  {
+    day: "Leeds Tuesday",
+    url: "/Leeds-Tue",
+  },
+  {
+    day: "Leeds Half Holiday",
+    url: "/Leeds-Wed",
+  },
+  {
+    day: "Leeds Thursday Vets",
+    url: "/Leeds-ThuVets",
+  },
+  {
+    day: "Leeds Saturday",
+    url: "/Leeds-Sat",
+  },
+  {
+    day: "Leeds Ladies",
+    url: "/LeedsLadies",
+  },
+  {
+    day: "Mirfield",
+    url: "/Mirfield",
+  },
+  {
+    day: "Spen Valley",
+    url: "/WestRiding",
+  },
+  {
+    day: "Ossett and Horbury",
+    url: "/Ossett",
+  },
+  {
+    day: "AireWharfe Monday",
+    url: "/AW-Mon",
+  },
+  {
+    day: "AireWharfe Tuesday Pairs",
+    url: "/AW-TuePairs",
+  },
+  {
+    day: "AireWharfe Wednesday Pairs",
+    url: "/AW-WedPairs",
+  },
+  {
+    day: "AireWharfe Vets",
+    url: "/AW-Vets",
+  },
+  {
+    day: "AireWharfe Wednesday",
+    url: "/AW-WedSingles",
+  },
+  {
+    day: "AireWharfe Saturday",
+    url: "/AW-Sat",
+  },
+  {
+    day: "Bradford Monday",
+    url: "/Bradford-Mon",
+  },
+  {
+    day: "Bradford Half Holiday",
+    url: "/Bradford-HalfHol",
+  },
+  {
+    day: "Bradford Vets",
+    url: "/Bradford-Vets",
+  },
+  {
+    day: "Bradford Pairs",
+    url: "/Bradford-Pairs",
+  },
+  {
+    day: "Bradford Saturday",
+    url: "/Bradford-Sat",
+  },
+  {
+    day: "Bradford Ladies",
+    url: "/BradfordLadies",
+  },
+  {
+    day: "Tadcaster",
+    url: "/Tadcaster",
+  },
+  {
+    day: "Tadcaster Vets",
+    url: "/TadcasterVets",
+  },
+  {
+    day: "North East Leeds Vets",
+    url: "/NELeedsVets",
+  },
+  {
+    day: "Barkston Ash",
+    url: "/BarkstonAsh",
+  },
+  {
+    day: "Heavy Woolen Afternoon",
+    url: "/HeavyWoollenAfternoon",
+  },
+  {
+    day: "Heavy Woolen Evening",
+    url: "/HeavyWoollenEvening",
+  },
+  {
+    day: "Harrogate",
+    url: "/Harrogate",
+  },
+  {
+    day: "Harrogate Vets",
+    url: "/HarrogateVets",
+  },
+  {
+    day: "Crossgates",
+    url: "/Crossgates",
+  },
+  {
+    day: "Elland",
+    url: "/Halifax-Elland",
+  },
+  {
+    day: "Halifax Tuesday Afternoon",
+    url: "/Halifax-TueAfternoon",
+  },
+  {
+    day: "Halifax Tuesday Evening",
+    url: "/Halifax-TueEvening",
+  },
+  {
+    day: "Halifax Wednesday",
+    url: "/Halifax-WedEvening",
+  },
+  {
+    day: "Halifax Thursday Afternoon",
+    url: "/Halifax-ThuAfternoon",
+  },
+  {
+    day: "Halifax Ladies Vets",
+    url: "/Halifax-LadiesO55",
+  },
+  {
+    day: "Hebden Royd Friday",
+    url: "/Halifax-HRFridays",
+  },
+  {
+    day: "Hebden Royd Evening",
+    url: "/Halifax-HREvening",
+  },
+  {
+    day: "Hebden Royd Vets",
+    url: "/Halifax-HRVets",
+  },
+  {
+    day: "Huddersfield",
+    url: "/Huddersfield",
+  },
+  {
+    day: "Huddersfield Works",
+    url: "/HuddWorks",
+  },
+  {
+    day: "Huddersfield Ladies Works",
+    url: "/HuddLWorks",
+  },
+  {
+    day: "Huddersfield Ladies",
+    url: "/HuddLadies",
+  },
+  {
+    day: "Huddersfield Ladies Afternoon",
+    url: "/HuddLadiesA",
+  },
+  {
+    day: "Huddersfield Libs",
+    url: "/HuddLibs",
+  },
+  {
+    day: "Huddersfield Vets 10",
+    url: "/HuddVets-10Man",
+  },
+  {
+    day: "Huddersfield Vets 6",
+    url: "/HuddVets-6Man",
+  },
+  {
+    day: "Huddersfield Junior",
+    url: "/HuddJunior",
+  },
+  {
+    day: "Brighouse Ladies",
+    url: "/Brighouse-Ladies",
+  },
+  {
+    day: "Brighouse Vets",
+    url: "/Brighouse-Vets",
+  },
+  {
+    day: "Brighouse Ladies Vets",
+    url: "/Brighouse-LVets",
+  },
+  {
+    day: "Castleford Evening",
+    url: "/Castleford-Evening",
+  },
+  {
+    day: "Castleford Pairs",
+    url: "/Castleford-Pairs",
+  },
+  {
+    day: "Castleford Vets Pairs",
+    url: "/CastlefordVets-Pairs",
+  },
+  {
+    day: "Castleford Vets Singles",
+    url: "/CastlefordVets-Singles",
+  },
+  {
+    day: "Wakefield Saturday",
+    url: "/WakefieldSat",
+  },
+  {
+    day: "Wakefield Vets",
+    url: "/WakefieldVets",
+  },
+  {
+    day: "Wakefield Workshops",
+    url: "/WakefieldWorkshops",
+  },
+  {
+    day: "Dewsbury Vets",
+    url: "/DewsburyVets",
+  },
+  {
+    day: "North Kirklees Vets",
+    url: "/NKirklees-Vets",
+  },
+  {
+    day: "North Kirklees Pairs",
+    url: "/NKirklees-Pairs",
+  },
+  {
+    day: "Spen Vets",
+    url: "/SpenVets",
+  },
+  {
+    day: "Skipton 54321",
+    url: "/Skipton-54321",
+  },
+  {
+    day: "Skipton Merit",
+    url: "/Skipton-Merit",
+  },
+  {
+    day: "Yorkshire Vets",
+    url: "/YorkshireVets",
+  },
+  {
+    day: "Yorkshire Ladies",
+    url: "/YorkshireLP",
+  },
+];
